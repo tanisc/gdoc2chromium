dd85e2b9228e668ca7fd021c2a7f82c32d01ccd3
tanisc <tanisc@users.noreply.github.com>
2017-02-27 00:46:36 +0200

    Update install_binary.sh

--- install_binary.sh
+++ install_binary.sh
@@ -1,9 +1,6 @@
 sudo mkdir /usr/share/gdoc2chromium
 sudo cp gdoc2chromium /usr/share/gdoc2chromium/gdoc2chromium
 sudo cp gdoc2chromium.desktop /usr/share/applications/gdoc2chromium.desktop
-rm build -R
-rm dist -R
-rm gdoc2chromium.spec
 sudo cp gdoc.xml /usr/share/mime/packages/
 sudo cp gslide.xml /usr/share/mime/packages/
 sudo cp gsheet.xml /usr/share/mime/packages/
