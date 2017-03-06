323ef0f023f3079390d4c36f2fb437f349b41b07
tanisc <tanisc@users.noreply.github.com>
2017-03-06 19:38:13 +0200

    Update install_binary.sh

--- install_binary.sh
+++ install_binary.sh
@@ -2,11 +2,11 @@ sudo mkdir /usr/share/gdoc2chromium
 sudo cp gdoc2chromium /usr/share/gdoc2chromium/gdoc2chromium
 sudo cp gdoc2chromium.desktop /usr/share/applications/gdoc2chromium.desktop
 sudo cp gdoc.xml /usr/share/mime/packages/
-sudo cp gslide.xml /usr/share/mime/packages/
+sudo cp gslides.xml /usr/share/mime/packages/
 sudo cp gsheet.xml /usr/share/mime/packages/
 sudo cp gform.xml /usr/share/mime/packages/
 sudo update-mime-database /usr/share/mime
 echo "application/x-gdoc=gdoc2chromium.desktop" >> $HOME/.local/share/applications/defaults.list
 echo "application/x-gsheet=gdoc2chromium.desktop" >> $HOME/.local/share/applications/defaults.list
-echo "application/x-gslide=gdoc2chromium.desktop" >> $HOME/.local/share/applications/defaults.list
+echo "application/x-gslides=gdoc2chromium.desktop" >> $HOME/.local/share/applications/defaults.list
 echo "application/x-gform=gdoc2chromium.desktop" >> $HOME/.local/share/applications/defaults.list
